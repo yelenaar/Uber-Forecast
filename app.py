ff7dccada0c6b26b57f05913c722fe170da066f4
Yelena Wu <yelenawu@yelena-wus-air.att.net>
2014-06-01 18:31:34 -0700

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import datetime
 import json
 
 from flask import Flask
 app = Flask(__name__)
+INPUT_TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S+00:00"
 
 @app.route('/<int:day_int>')
 def hello(day_int):
 	return json.dumps(day_int)
+
+def brain_on(filename):
+	with open(filename) as f:
+		data = json.load()
+	for index in range(len(data)):
+		data[index] = utc_to_epoch(data[index])
+	del data[:]
+
+def utc_to_epoch(uat_timestamp):
+	date.strptime)uat_timestamp, INPUT_TIMESTAMP_FORMAT).strftime('%s')
 
 if __name__ == "__main__":
 	app.run()
--- ucast/__init__.py
+++ ucast/__init__.py
@@ -1,17 +1,6 @@
 __author__ = 'Yelena Wu (yelenamqwu@gmailcom)'
 
-import datetime
-import json
-import pprint
 
-INPUT_TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S+00:00"
+class UberLogin(object):
+	def __init__(self, data):
 
-def brain_on(filename):
-	with open(filename) as f:
-		data = json.load()
-	for index in range(len(data)):
-		data[index] = utc_to_epoch(data[index])
-	del data[:]
-
-def utc_to_epoch(uat_timestamp):
-	date.strptime)uat_timestamp, INPUT_TIMESTAMP_FORMAT).strftime('%s')
